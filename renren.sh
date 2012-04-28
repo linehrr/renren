ac167f733e3ebbaf948e8f340369861685ea4644
linehrr <linehrr@gmail.com>
2012-04-28 20:31:07 -0300

    Update renren.sh

--- renren.sh
+++ renren.sh
@@ -3,7 +3,7 @@
 for i in $(seq 1000); do 
 	rm home* 
 
-	wget --header="Cookie:_r01_=1; mop_uniq_ckid=127.0.0.1_1328239655_1437844104; _de=DA1307199A949EED060571BA34DEDCB5; anonymid=h0y5rgrfkur79t; __utma=151146938.619555353.1334952316.1334952316.1334952316.1; __utmz=151146938.1334952316.1.1.utmcsr=lover.renren.com|utmccn=(referral)|utmcmd=referral|utmcct=/600486217; depovince=GW; jebecookies=01e0d277-4474-4797-914f-c756b0fce474|||||; XNESSESSIONID=abdbd9258e43; at=1; p=e3b174bd49c5760b7279bed5a9b0f5b86; ap=237725526; t=a24c0da24ab5d50b7235f6183f8b81ee6; societyguester=a24c0da24ab5d50b7235f6183f8b81ee6; id=237725526; xnsid=1023b207; vip=1; loginfrom=null" http://www.renren.com/home 
+	wget --header="Cookie:_r01_=1; mop_uniq_ckid=127.0.0.1_1328239655_1437844104; anonymid=h0y5rgrfkur79t; _de=DA1307199A949EED060571BA34DEDCB5; __utma=10481322.350472199.1335541232.1335541232.1335545492.2; __utmz=10481322.1335545492.2.2.utmcsr=renren.com|utmccn=(referral)|utmcmd=referral|utmcct=/237725526; depovince=GW; at=1; jebecookies=43af11f8-982d-4ff1-a5e4-7ad7f8493729|||||; p=85be9a082d81ccf152230aa5e06905176; ap=237725526; t=40849ae241b52d797065ae45fe0d57bc6; societyguester=40849ae241b52d797065ae45fe0d57bc6; id=237725526; xnsid=5e77e40; wpsid=13497917510200; vip=1; XNESSESSIONID=2496655a64ff; loginfrom=null" http://www.renren.com/home 
 
 	cat home | grep namecard > 1.txt 
 
@@ -14,7 +14,7 @@ for i in $(seq 1000); do
 		line=`echo ${line%\"*}` 
 		line=`echo ${line%\"\>\<*}` 
 		echo $line 
-		wget --spider --header="Cookie:_r01_=1; mop_uniq_ckid=127.0.0.1_1328239655_1437844104; _de=DA1307199A949EED060571BA34DEDCB5; anonymid=h0y5rgrfkur79t; __utma=151146938.619555353.1334952316.1334952316.1334952316.1; __utmz=151146938.1334952316.1.1.utmcsr=lover.renren.com|utmccn=(referral)|utmcmd=referral|utmcct=/600486217; depovince=GW; jebecookies=01e0d277-4474-4797-914f-c756b0fce474|||||; XNESSESSIONID=abdbd9258e43; at=1; p=e3b174bd49c5760b7279bed5a9b0f5b86; ap=237725526; t=a24c0da24ab5d50b7235f6183f8b81ee6; societyguester=a24c0da24ab5d50b7235f6183f8b81ee6; id=237725526; xnsid=1023b207; vip=1; loginfrom=null" $line 
+		wget --spider --header="Cookie:_r01_=1; mop_uniq_ckid=127.0.0.1_1328239655_1437844104; anonymid=h0y5rgrfkur79t; _de=DA1307199A949EED060571BA34DEDCB5; __utma=10481322.350472199.1335541232.1335541232.1335545492.2; __utmz=10481322.1335545492.2.2.utmcsr=renren.com|utmccn=(referral)|utmcmd=referral|utmcct=/237725526; depovince=GW; at=1; jebecookies=43af11f8-982d-4ff1-a5e4-7ad7f8493729|||||; p=85be9a082d81ccf152230aa5e06905176; ap=237725526; t=40849ae241b52d797065ae45fe0d57bc6; societyguester=40849ae241b52d797065ae45fe0d57bc6; id=237725526; xnsid=5e77e40; wpsid=13497917510200; vip=1; XNESSESSIONID=2496655a64ff; loginfrom=null" $line 
 
 	done 
 	echo $i 
